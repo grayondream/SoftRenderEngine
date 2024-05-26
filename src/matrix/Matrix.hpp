d435ae76fb9cfbeb965ba35dfb0a766288c8448a
grayondream <grayondream@outlook.com>
2024-05-26 12:06:30 +0800

    feat: Matrix2D string done

--- src/matrix/Matrix.hpp
+++ src/matrix/Matrix.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <algorithm>
 #include <initializer_list>
 #include <vector>
 
@@ -14,49 +15,63 @@ protected:
 };
 
 template<class T>
+class Matrix1DBase;
+
+template<class T>
 class MatrixIndex1 : public MatrixIndexBase<T>{
 public:
-    MatrixIndex1(T* data) : MatrixIndexBase<T>(data){ }
+    MatrixIndex1(T* data, const MatrixSizeType d1) 
+        : MatrixIndexBase<T>(data), m_d1(d1){ }
     
     T& operator[](const MatrixSizeType idx) const{
         return *(this->m_pstart + idx);
     }
+
+    operator Matrix1DBase<T>(){
+        return Matrix1DBase<T>(this->m_pstart, this->m_d1);
+    }
+public:
+    MatrixSizeType m_d1{};
 };
 
 //d1 -> width
 template<class T>
 class MatrixIndex2 : public MatrixIndex1<T>{
 public:
-    MatrixIndex2(T* data, const MatrixSizeType d1) : MatrixIndex1<T>(data), m_d1(d1){ }
+    MatrixIndex2(T* data, const MatrixSizeType d2, const MatrixSizeType d1) 
+        : MatrixIndex1<T>(data, d1), m_d2(d2){ }
 
-    MatrixIndex1<T> operator[](const MatrixSizeType idx){
-        return MatrixIndex1<T>(this->m_pstart + idx * this->m_d1);
-    }
-protected:
-    MatrixSizeType m_d1{};
-};
-
-template<class T>
-class MatrixIndex3 : public MatrixIndex2<T>{
-public:
-    MatrixIndex3(T* data, const MatrixSizeType d2, const MatrixSizeType d1) : MatrixIndex2<T>(data, d1), m_d2(d2){ }
-
-    MatrixIndex2<T> operator[](const MatrixSizeType idx) {
-        return MatrixIndex2<T>(this->m_pstart + idx * this->m_d1 * this->m_d2);
+    MatrixIndex1<T> operator[](const MatrixSizeType idx) const{
+        return MatrixIndex1<T>(this->m_pstart + idx * this->m_d1, this->m_d1);
     }
 public:
     MatrixSizeType m_d2{};
 };
 
 template<class T>
-class MatrixIndex4 : public MatrixIndex3<T>{
+class MatrixIndex3 : public MatrixIndex2<T>{
 public:
-    MatrixIndex4(T* data, const MatrixSizeType d3, const MatrixSizeType d2, const MatrixSizeType d1) : MatrixIndex3<T>(data, d2, d1), m_d3(d3){}
-    MatrixIndex3<T> operator[](const MatrixSizeType idx) {
-        return MatrixIndex3<T>(this->m_pstart + idx * this->m_d1 * this->m_d2 * this->m_d3);
+    MatrixIndex3(T* data, const MatrixSizeType d3, const MatrixSizeType d2, const MatrixSizeType d1) 
+        : MatrixIndex2<T>(data, d2, d1), m_d3(d3){ }
+
+    MatrixIndex2<T> operator[](const MatrixSizeType idx) const {
+        return MatrixIndex2<T>(this->m_pstart + idx * this->m_d1 * this->m_d2);
     }
 public:
     MatrixSizeType m_d3{};
+};
+
+template<class T>
+class MatrixIndex4 : public MatrixIndex3<T>{
+public:
+    MatrixIndex4(T* data, const MatrixSizeType d4, const MatrixSizeType d3, const MatrixSizeType d2, const MatrixSizeType d1) 
+            : MatrixIndex3<T>(data, d3, d2, d1), m_d4(d4){}
+
+    MatrixIndex3<T> operator[](const MatrixSizeType idx) const {
+        return MatrixIndex3<T>(this->m_pstart + idx * this->m_d1 * this->m_d2 * this->m_d3);
+    }
+public:
+    MatrixSizeType m_d4{};
 };
 
 template<class T>
@@ -69,13 +84,23 @@ public:
         m_data.reserve(sz);
     }
 
-    MatrixBase(const std::initializer_list<T> &ls) : m_data(ls){}
+    MatrixBase(const std::initializer_list<T> &ls) 
+        : m_data(ls){}
+
+    MatrixBase(const T* data, const std::size_t size){
+        m_data.reserve(size);
+        std::copy_n(data, size, std::end(m_data));
+    }
+
+    MatrixBase(const std::vector<T> &vec){
+        m_data = vec;
+    }
 public:
     MatrixValueType* getRawBuffer(){
         return m_data.data();
     }
 
-    MatrixSizeType size(){
+    MatrixSizeType size() const{
         return m_data.size();
     }
 protected:
@@ -86,36 +111,58 @@ protected:
 template<class T>
 class Matrix1DBase : public MatrixBase<T>, public MatrixIndex1<T>{
 public:
-    Matrix1DBase(const MatrixSizeType d1) : MatrixBase<T>(d1), MatrixIndex1<T>(this->getRawBuffer()), m_d1(d1){}
-    Matrix1DBase(const std::initializer_list<T> &ls) : MatrixBase<T>(ls), MatrixIndex1<T>(this->getRawBuffer()), m_d1(ls.size()){}
+    Matrix1DBase(const MatrixSizeType d1) 
+        : MatrixBase<T>(d1), MatrixIndex1<T>(this->getRawBuffer(), d1){}
+    
+    Matrix1DBase(const std::initializer_list<T> &ls) 
+        : MatrixBase<T>(ls), MatrixIndex1<T>(this->getRawBuffer(), ls.size()){}
 
-public:
-    MatrixSizeType m_d1;
+    Matrix1DBase(const T* data, const MatrixSizeType d1)
+        : MatrixBase<T>(data, d1), MatrixIndex1<T>(this->getRawBuffer(), d1){}
+
+    Matrix1DBase(const std::vector<T> &vec)
+        :MatrixBase<T>(vec), MatrixIndex1<T>(this->getRawBuffer(), vec.size()){}
 };
 
 template<class T>
 class Matrix2DBase : public MatrixBase<T>, public MatrixIndex2<T>{
 public:
-    Matrix2DBase(const MatrixSizeType d2, const MatrixSizeType d1) : MatrixBase<T>(d1 * d2), MatrixIndex2<T>(this->getRawBuffer(), d2, d1), m_d2(d2){}
-public:
-    MatrixSizeType m_d2{};
-};
+    Matrix2DBase(const MatrixSizeType d2, const MatrixSizeType d1) 
+        : MatrixBase<T>(d1 * d2), MatrixIndex2<T>(this->getRawBuffer(), d2, d1, d2){} 
 
-template<class T>
-class Matrix3DBase : public MatrixBase<T>, public MatrixIndex3<T>{
+    Matrix2DBase(const std::initializer_list<T> &ls, const MatrixSizeType d2, const MatrixSizeType d1) 
+        : MatrixBase<T>(ls), MatrixIndex2<T>(this->getRawBuffer(), d2, d1){
+        assert(this->m_d2 * this->m_d1 == ls.size()); 
+    } 
+
+    Matrix2DBase(const T* data, const MatrixSizeType d2, const MatrixSizeType d1)
+        : MatrixBase<T>(data, d2 * d1), MatrixIndex2<T>(this->getRawBuffer(), d2, d1){}
+
+    Matrix2DBase(const std::vector<T> &vec, const MatrixSizeType d2, const MatrixSizeType d1)
+        : MatrixBase<T>(vec), MatrixIndex2<T>(this->getRawBuffer(), d2, d1){}
+}; 
+
+template<class T> class Matrix3DBase : public MatrixBase<T>, public MatrixIndex3<T>{
 public:
-    Matrix3DBase(const MatrixSizeType d3, const MatrixSizeType d2, const MatrixSizeType d1) : MatrixBase<T>(d1 * d2 * d3), MatrixIndex3<T>(this->getRawBuffer(), d3, d2, d1), m_d3(d3){}
-public:
-    MatrixSizeType m_d3{};
+    Matrix3DBase(const MatrixSizeType d3, const MatrixSizeType d2, const MatrixSizeType d1) 
+    : MatrixBase<T>(d1 * d2 * d3), MatrixIndex3<T>(this->getRawBuffer(), d3, d2, d1){}
+
+    Matrix3DBase(const std::initializer_list<T> &ls, const MatrixSizeType d3, const MatrixSizeType d2, const MatrixSizeType d1)
+        : MatrixBase<T>(ls), MatrixIndex3<T>(this->getRawBuffer(), d3, d2, d1){
+        assert(d3 * d2 * d1 == ls.size());
+    }
 };
 
 template<class T>
 class Matrix4DBase : public MatrixBase<T>, public MatrixIndex4<T>{
 public:
-    Matrix4DBase(const MatrixSizeType d4, const MatrixSizeType d3, const MatrixSizeType d2, const MatrixSizeType d1) :MatrixBase<T>(d1 * d2 * d3 * d4),MatrixIndex4<int>(this->getRawBuffer(), d4, d3, d2, d1), m_d4(d4){
+    Matrix4DBase(const MatrixSizeType d4, const MatrixSizeType d3, const MatrixSizeType d2, const MatrixSizeType d1) 
+        : MatrixBase<T>(d1 * d2 * d3 * d4),MatrixIndex4<int>(this->getRawBuffer(), d4, d3, d2, d1){ }
+
+    Matrix4DBase(const std::initializer_list<T> &ls, const MatrixSizeType d4, const MatrixSizeType d3, const MatrixSizeType d2, const MatrixSizeType d1)
+        : MatrixBase<T>(d4 * d3 * d2 * d1), MatrixIndex4<T>(this->getRawBuffer(), d4, d3, d2, d1){
+        assert(ls.size() == d4 * d3 * d2 * d1);
     }
-public:
-    MatrixSizeType m_d4{};
 };
 
 using Matrix1D = Matrix1DBase<double>;
--- test/matrix/MatrixBase.cpp
+++ test/matrix/MatrixBase.cpp
@@ -2,28 +2,53 @@
 #include "Log.hpp"
 #include "MatrixUtils.hpp"
 #include <algorithm>
+#include <cstddef>
 #include <gtest/gtest.h>
+#include <type_traits>
+
+static void LogMatrix1D(const Matrix1D &mat, const std::string tag = {}){
+    LOGI("Matrix {} total size: {}, d1 = {}\n{}", tag, mat.size(), mat.m_d1, std::to_string(mat));
+}
+
+static void LogMatrix2D(const Matrix2D &mat, const std::string tag = {}){
+    LOGI("Matrix {} total size: {}, d1 = {}, d2 = {}\n{}", tag, mat.size(), mat.m_d1, mat.m_d2, std::to_string(mat));
+}
+
+template<class T>
+static void LogMatrix(const T &mat, const std::string tag = {}){
+    if constexpr(std::is_same_v<T, Matrix1D>){
+        return LogMatrix1D(mat, tag);
+    }else if constexpr(std::is_same_v<T, Matrix2D>){
+        return LogMatrix2D(mat, tag);
+    }
+}
 
 TEST(Matrix1DTest, CreateAndPrint){
     LOGI("Testing Matrix1D base ability");
-    const int n1 = 5, n2 = 10, n3 = 20;
+    const int n1 = 5, n2 = 7, n3 = 10;
     Matrix1D m1(n1);
     Matrix1D m2(n2);
     Matrix1D m3(n3);
     std::fill_n(m1.getRawBuffer(), n1, 3.3);
     std::fill_n(m2.getRawBuffer(), n2, 2.2);
     std::fill_n(m3.getRawBuffer(), n3, 7.7);
-    LOGI("m1 string: {}", std::to_string(m1));
-    LOGI("m2 string: {}", std::to_string(m2));
-    LOGI("m3 string: {}", std::to_string(m3));
-
-    //initialize matrix with initalize list
     Matrix1D m4({1,2,3,4,5,6,7,8,9,10,11}) ;
-    LOGI("m4 size is {}, content {}", m4.size(), std::to_string(m4));
+    LogMatrix(m1);
+    LogMatrix(m2);
+    LogMatrix(m3);
+    LogMatrix(m4);
 }
 
 TEST(Matrix2DTest, CreateAndPrint){
     LOGI("Testing Matrix2D base ability");
+    const std::size_t n1 = 25, n2 = 49, n3 = 100;
+    std::vector<double> vec1(n1, 10), vec2(n2, 11), vec3(n3, 22);
+    Matrix2D m1(vec1, 5, 5);
+    Matrix2D m2(vec2, 7, 7);
+    Matrix2D m3(vec3, 10, 10);
+    LogMatrix(m1);
+    LogMatrix(m2);
+    LogMatrix(m3);
 };
 
 int main(int argc, char **argv){

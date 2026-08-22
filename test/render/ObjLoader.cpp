79098c55ca26cd5d22387261f5d3270bcc09e826
grayondream <grayondream@outlook.com>
2026-08-23 01:33:39 +0800

    feat(render): wavefront OBJ loader with flat-normal fallback and cube asset

--- test/render/ObjLoader.cpp
+++ test/render/ObjLoader.cpp
@@ -1,4 +1,8 @@
 #include "ObjLoader.hpp"
+#include "Rasterizer.hpp"
+#include "Pipeline.hpp"
+#include "Transform.hpp"
+#include "Texture.hpp"
 #include <gtest/gtest.h>
 #include <cstdio>
 #include <cmath>
@@ -68,6 +72,86 @@ TEST(ObjTest, NegativeIndices){
     ASSERT_TRUE(loadObjFromFile(TmpPath("neg.obj"), obj));
     EXPECT_EQ(obj.numPolys, 1);
     EXPECT_DOUBLE_EQ(obj.plist[0].vlist[2].y, 1.0);
+}
+
+TEST(ObjTest, MissingNormalsFlatComputed){
+    ASSERT_TRUE(WriteFile(TmpPath("nonorm.obj"),
+        "v 0 0 0\nv 1 0 0\nv 0 0 -1\nf 1 2 3\n"));
+    Object4D obj{};
+    ASSERT_TRUE(loadObjFromFile(TmpPath("nonorm.obj"), obj));
+    const auto &n = obj.plist[0].nlist[0];
+    EXPECT_NEAR(n.x, 0.0, 1e-12);
+    EXPECT_NEAR(n.y, 1.0, 1e-12);
+    EXPECT_NEAR(n.z, 0.0, 1e-12);
+    EXPECT_TRUE(n == obj.plist[0].nlist[1]);
+    EXPECT_TRUE(n == obj.plist[0].nlist[2]);
+}
+
+TEST(ObjTest, MissingUvsZeroed){
+    ASSERT_TRUE(WriteFile(TmpPath("nouvs.obj"), "v 0 0 0\nv 1 0 0\nv 0 1 0\nf 1 2 3\n"));
+    Object4D obj{};
+    ASSERT_TRUE(loadObjFromFile(TmpPath("nouvs.obj"), obj));
+    EXPECT_DOUBLE_EQ(obj.plist[0].uvlist[0].u, 0.0);
+    EXPECT_DOUBLE_EQ(obj.plist[0].uvlist[2].v, 0.0);
+}
+
+TEST(ObjTest, CapacityExceededFails){
+    std::string content;
+    for(int i = 0; i < 65; i++){
+        content += "v 0 0 0\n";
+    }
+    ASSERT_TRUE(WriteFile(TmpPath("cap.obj"), content));
+    Object4D obj{};
+    EXPECT_FALSE(loadObjFromFile(TmpPath("cap.obj"), obj));
+    EXPECT_EQ(obj.numVertices, 0);
+    EXPECT_EQ(obj.numPolys, 0);
+}
+
+TEST(ObjTest, BadTokenFails){
+    ASSERT_TRUE(WriteFile(TmpPath("bad.obj"), "v x 0 0\n"));
+    Object4D obj{};
+    EXPECT_FALSE(loadObjFromFile(TmpPath("bad.obj"), obj));
+}
+
+TEST(ObjTest, MissingFileFails){
+    Object4D obj{};
+    EXPECT_FALSE(loadObjFromFile("/tmp/opencode/no_such_12345.obj", obj));
+}
+
+TEST(ObjTest, RealCubeAsset){
+    Object4D obj{};
+    ASSERT_TRUE(loadObjFromFile("assets/cube.obj", obj));
+    EXPECT_EQ(obj.numVertices, 8);
+    EXPECT_EQ(obj.numPolys, 12);
+    EXPECT_DOUBLE_EQ(obj.plist[0].nlist[0].z, -1.0);
+    EXPECT_DOUBLE_EQ(obj.plist[1].nlist[0].z, -1.0);
+}
+
+TEST(ObjTest, EndToEndRendersPixels){
+    Object4D obj{};
+    ASSERT_TRUE(loadObjFromFile("assets/cube.obj", obj));
+
+    auto model = SGE::Math::rotationY(0.6).mul(SGE::Math::rotationX(0.4));
+    auto view = SGE::Math::lookAt(Vector3DBase<double>{0, 2, -6},
+                                  Vector3DBase<double>{0, 0, 0},
+                                  Vector3DBase<double>{0, 1, 0});
+    auto proj = SGE::Math::perspective(M_PI/3, 800.0/600.0, 0.1, 100.0);
+    auto vp = proj.mul(view);
+    auto nrm = SGE::Math::normalMatrix(model);
+
+    FrameBuffer fb(800, 600);
+    Rasterizer rz{fb};
+    uint32_t px[1] = {0xFFFFFFFFu};
+    Texture tex(1, 1, px);
+    for(auto &t : Pipeline::projectObject(obj, model, vp, nrm, 800, 600)){
+        rz.drawTriangleTextured(t.v[0], t.v[1], t.v[2], tex);
+    }
+
+    std::size_t lit = 0;
+    for(std::size_t i = 0; i < fb.width() * fb.height(); i++){
+        if(fb.colorData()[i] != 0xFF000000u) lit++;
+    }
+    EXPECT_GT(lit, 1000u);
 }
 
 int main(int argc, char **argv){
